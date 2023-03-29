c9ba7793a8232d112d6345e8501c5036572ade09
Jlegnaghi <jeffersonlegnaghi07@gmail.com>
2023-03-29 18:54:56 -0300

    tentando fazer conexao

---
+++ Autenticacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDC_Cracha
+{
+    static class Autenticacao
+    {
+        static string nome;
+        static string senha;
+        static int nivel;
+
+        public static void login(string nome1, string senha1, int nivel1) 
+        {
+            nome =nome1;
+            senha = senha1;
+            nivel = nivel1;
+        }
+        public static void logout()
+        {
+            nome = null;
+            senha = null;
+            nivel = 0;
+        }
+
+        public static string getUsuario() 
+        {
+            return "Nome: " + nome + "\nSenha: " + senha + "\nNivel: " + nivel;
+        }
+    }
+}
---
+++ Conexao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace UDC_Cracha
+{
+    internal class Conexao
+    {
+        public OleDbConnection cn = new OleDbConnection(); 
+
+        public void conectar()
+        {
+            cn.ConnectionString = "Integrated Security=SSPI;Persist Security Info=False;Data Source=Legnaghi";
+            cn.Open();
+        }
+        public void desconectar()
+        {
+            cn.Close();
+        }
+
+    }
+}
